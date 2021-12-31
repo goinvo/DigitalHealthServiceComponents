e361a6bb9c39abc8572e05e8d78f4d7cc8c39e2a
Victor Hung <victorhung92@gmail.com>
2021-12-31 01:16:32 -0500

    add text description for layers

--- web/js/app.js
+++ web/js/app.js
@@ -14,6 +14,192 @@ var RootElement = function RootElement() {
           isLastChild: key === data.stack.length - 1
         });
       })
+    ),
+    React.createElement(
+      "div",
+      { className: "description-container" },
+      React.createElement(
+        "div",
+        { className: "layer-description" },
+        React.createElement(
+          "p",
+          null,
+          React.createElement(
+            "strong",
+            null,
+            "Patient Data Use Agreement (pDUA)"
+          ),
+          " is a contract between the patient and a service provider that states in clear, simple language or graphics, how data will be used and accessed. The pDUA is evidence for patients of how the service provider's will use, or not use, their information. More than just terms & conditions, the pDUA gives options for the terms of the agreement itself. It allows everyone to assess the trustworthiness and practices of a third party service. Options also include how data is transitioned, if the agreement is terminated. Patients need to be on the same legal altitude as healthcare services, apps, and hospitals. A binding data contract, governed by the patients, is critical to putting patients in control and protecting patients from corporate overreach."
+        ),
+        React.createElement(
+          "h2",
+          null,
+          "References:"
+        ),
+        React.createElement(
+          "p",
+          null,
+          React.createElement(
+            "a",
+            { href: "https://datauseagreement.org" },
+            "https://datauseagreement.org"
+          )
+        ),
+        React.createElement(
+          "h2",
+          null,
+          "1 pager:"
+        ),
+        React.createElement(
+          "p",
+          null,
+          React.createElement(
+            "a",
+            { href: "https://healthcareitnews.com/news/why-patients-and-providers-need-model-patient-data-use-agreement" },
+            "https://www.healthcareitnews.com/news/why-patients-and-providers-need-model-patient-data-use-agreement"
+          )
+        )
+      ),
+      React.createElement(
+        "div",
+        { className: "layer-description" },
+        React.createElement(
+          "p",
+          null,
+          React.createElement(
+            "strong",
+            null,
+            "Patient Data Manager (PDM)"
+          ),
+          " is a digital service working on behalf of a patient to continually find, retrieve, add, edit, correct, and normalize data and construct a person's longitudinal health record. Because a patient's data comes from many places, the PDM takes it and puts it into the same place (one, comprehensive health record)."
+        )
+      ),
+      React.createElement(
+        "div",
+        { className: "layer-description" },
+        React.createElement(
+          "p",
+          null,
+          React.createElement(
+            "strong",
+            null,
+            "Patient Data Manager (PDM)"
+          ),
+          " is a digital service working on behalf of a patient to continually find, retrieve, add, edit, correct, and normalize data and construct a person's longitudinal health record. Because a patient's data comes from many places, the PDM takes it and puts it into the same place (one, comprehensive health record)."
+        )
+      ),
+      React.createElement(
+        "div",
+        { className: "layer-description" },
+        React.createElement(
+          "p",
+          null,
+          React.createElement(
+            "strong",
+            null,
+            "Patient Data Manager (PDM)"
+          ),
+          " is a digital service working on behalf of a patient to continually find, retrieve, add, edit, correct, and normalize data and construct a person's longitudinal health record. Because a patient's data comes from many places, the PDM takes it and puts it into the same place (one, comprehensive health record)."
+        )
+      ),
+      React.createElement(
+        "div",
+        { className: "layer-description" },
+        React.createElement(
+          "p",
+          null,
+          React.createElement(
+            "strong",
+            null,
+            "Patient Data Manager (PDM)"
+          ),
+          " is a digital service working on behalf of a patient to continually find, retrieve, add, edit, correct, and normalize data and construct a person's longitudinal health record. Because a patient's data comes from many places, the PDM takes it and puts it into the same place (one, comprehensive health record)."
+        )
+      ),
+      React.createElement(
+        "div",
+        { className: "layer-description" },
+        React.createElement(
+          "p",
+          null,
+          React.createElement(
+            "strong",
+            null,
+            "Patient Data Manager (PDM)"
+          ),
+          " is a digital service working on behalf of a patient to continually find, retrieve, add, edit, correct, and normalize data and construct a person's longitudinal health record. Because a patient's data comes from many places, the PDM takes it and puts it into the same place (one, comprehensive health record)."
+        )
+      ),
+      React.createElement(
+        "div",
+        { className: "layer-description" },
+        React.createElement(
+          "p",
+          null,
+          React.createElement(
+            "strong",
+            null,
+            "Patient Data Manager (PDM)"
+          ),
+          " is a digital service working on behalf of a patient to continually find, retrieve, add, edit, correct, and normalize data and construct a person's longitudinal health record. Because a patient's data comes from many places, the PDM takes it and puts it into the same place (one, comprehensive health record)."
+        )
+      ),
+      React.createElement(
+        "div",
+        { className: "layer-description" },
+        React.createElement(
+          "p",
+          null,
+          React.createElement(
+            "strong",
+            null,
+            "Patient Data Manager (PDM)"
+          ),
+          " is a digital service working on behalf of a patient to continually find, retrieve, add, edit, correct, and normalize data and construct a person's longitudinal health record. Because a patient's data comes from many places, the PDM takes it and puts it into the same place (one, comprehensive health record)."
+        )
+      ),
+      React.createElement(
+        "div",
+        { className: "layer-description" },
+        React.createElement(
+          "p",
+          null,
+          React.createElement(
+            "strong",
+            null,
+            "Patient Data Manager (PDM)"
+          ),
+          " is a digital service working on behalf of a patient to continually find, retrieve, add, edit, correct, and normalize data and construct a person's longitudinal health record. Because a patient's data comes from many places, the PDM takes it and puts it into the same place (one, comprehensive health record)."
+        )
+      ),
+      React.createElement(
+        "div",
+        { className: "layer-description" },
+        React.createElement(
+          "p",
+          null,
+          React.createElement(
+            "strong",
+            null,
+            "Patient Data Manager (PDM)"
+          ),
+          " is a digital service working on behalf of a patient to continually find, retrieve, add, edit, correct, and normalize data and construct a person's longitudinal health record. Because a patient's data comes from many places, the PDM takes it and puts it into the same place (one, comprehensive health record)."
+        )
+      ),
+      React.createElement(
+        "div",
+        { className: "layer-description" },
+        React.createElement(
+          "p",
+          null,
+          React.createElement(
+            "strong",
+            null,
+            "Patient Data Manager (PDM)"
+          ),
+          " is a digital service working on behalf of a patient to continually find, retrieve, add, edit, correct, and normalize data and construct a person's longitudinal health record. Because a patient's data comes from many places, the PDM takes it and puts it into the same place (one, comprehensive health record)."
+        )
+      )
     )
   );
 };
--- web/src/app.jsx
+++ web/src/app.jsx
@@ -14,6 +14,137 @@ const RootElement = () => {
           );
         })}
       </div>
+      <div className="description-container">
+        <div className="layer-description">
+          <p>
+            <strong>Patient Data Use Agreement (pDUA)</strong> is a contract
+            between the patient and a service provider that states in clear,
+            simple language or graphics, how data will be used and accessed. The
+            pDUA is evidence for patients of how the service provider's will
+            use, or not use, their information. More than just terms &
+            conditions, the pDUA gives options for the terms of the agreement
+            itself. It allows everyone to assess the trustworthiness and
+            practices of a third party service. Options also include how data is
+            transitioned, if the agreement is terminated. Patients need to be on
+            the same legal altitude as healthcare services, apps, and hospitals.
+            A binding data contract, governed by the patients, is critical to
+            putting patients in control and protecting patients from corporate
+            overreach.
+          </p>
+          <h2>References:</h2>
+          <p>
+            <a href="https://datauseagreement.org">
+              https://datauseagreement.org
+            </a>
+          </p>
+          <h2>1 pager:</h2>
+          <p>
+            <a href="https://healthcareitnews.com/news/why-patients-and-providers-need-model-patient-data-use-agreement">
+              https://www.healthcareitnews.com/news/why-patients-and-providers-need-model-patient-data-use-agreement
+            </a>
+          </p>
+        </div>
+        <div className="layer-description">
+          <p>
+            <strong>Patient Data Manager (PDM)</strong> is a digital service
+            working on behalf of a patient to continually find, retrieve, add,
+            edit, correct, and normalize data and construct a person's
+            longitudinal health record. Because a patient's data comes from many
+            places, the PDM takes it and puts it into the same place (one,
+            comprehensive health record).
+          </p>
+        </div>
+        <div className="layer-description">
+          <p>
+            <strong>Patient Data Manager (PDM)</strong> is a digital service
+            working on behalf of a patient to continually find, retrieve, add,
+            edit, correct, and normalize data and construct a person's
+            longitudinal health record. Because a patient's data comes from many
+            places, the PDM takes it and puts it into the same place (one,
+            comprehensive health record).
+          </p>
+        </div>
+        <div className="layer-description">
+          <p>
+            <strong>Patient Data Manager (PDM)</strong> is a digital service
+            working on behalf of a patient to continually find, retrieve, add,
+            edit, correct, and normalize data and construct a person's
+            longitudinal health record. Because a patient's data comes from many
+            places, the PDM takes it and puts it into the same place (one,
+            comprehensive health record).
+          </p>
+        </div>
+        <div className="layer-description">
+          <p>
+            <strong>Patient Data Manager (PDM)</strong> is a digital service
+            working on behalf of a patient to continually find, retrieve, add,
+            edit, correct, and normalize data and construct a person's
+            longitudinal health record. Because a patient's data comes from many
+            places, the PDM takes it and puts it into the same place (one,
+            comprehensive health record).
+          </p>
+        </div>
+        <div className="layer-description">
+          <p>
+            <strong>Patient Data Manager (PDM)</strong> is a digital service
+            working on behalf of a patient to continually find, retrieve, add,
+            edit, correct, and normalize data and construct a person's
+            longitudinal health record. Because a patient's data comes from many
+            places, the PDM takes it and puts it into the same place (one,
+            comprehensive health record).
+          </p>
+        </div>
+        <div className="layer-description">
+          <p>
+            <strong>Patient Data Manager (PDM)</strong> is a digital service
+            working on behalf of a patient to continually find, retrieve, add,
+            edit, correct, and normalize data and construct a person's
+            longitudinal health record. Because a patient's data comes from many
+            places, the PDM takes it and puts it into the same place (one,
+            comprehensive health record).
+          </p>
+        </div>
+        <div className="layer-description">
+          <p>
+            <strong>Patient Data Manager (PDM)</strong> is a digital service
+            working on behalf of a patient to continually find, retrieve, add,
+            edit, correct, and normalize data and construct a person's
+            longitudinal health record. Because a patient's data comes from many
+            places, the PDM takes it and puts it into the same place (one,
+            comprehensive health record).
+          </p>
+        </div>
+        <div className="layer-description">
+          <p>
+            <strong>Patient Data Manager (PDM)</strong> is a digital service
+            working on behalf of a patient to continually find, retrieve, add,
+            edit, correct, and normalize data and construct a person's
+            longitudinal health record. Because a patient's data comes from many
+            places, the PDM takes it and puts it into the same place (one,
+            comprehensive health record).
+          </p>
+        </div>
+        <div className="layer-description">
+          <p>
+            <strong>Patient Data Manager (PDM)</strong> is a digital service
+            working on behalf of a patient to continually find, retrieve, add,
+            edit, correct, and normalize data and construct a person's
+            longitudinal health record. Because a patient's data comes from many
+            places, the PDM takes it and puts it into the same place (one,
+            comprehensive health record).
+          </p>
+        </div>
+        <div className="layer-description">
+          <p>
+            <strong>Patient Data Manager (PDM)</strong> is a digital service
+            working on behalf of a patient to continually find, retrieve, add,
+            edit, correct, and normalize data and construct a person's
+            longitudinal health record. Because a patient's data comes from many
+            places, the PDM takes it and puts it into the same place (one,
+            comprehensive health record).
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
