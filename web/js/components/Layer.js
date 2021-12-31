46bf26b83689afa748ee94eecc99ce5748196b9b
Victor Hung <victorhung92@gmail.com>
2021-12-30 21:31:15 -0500

    add text labels

--- web/js/components/Layer.js
+++ web/js/components/Layer.js
@@ -4,7 +4,21 @@ var Layer = function Layer(_ref) {
   return React.createElement(
     "div",
     { className: "layer-container", style: { zIndex: zIndex } },
-    React.createElement("div", { className: "layer" })
+    React.createElement("div", { className: "layer" }),
+    React.createElement(
+      "div",
+      { className: "layer-text-container" },
+      React.createElement(
+        "div",
+        { className: "main" },
+        "Bioassay Explained"
+      ),
+      React.createElement(
+        "div",
+        { className: "subtext" },
+        "5th grade level, labs explanation"
+      )
+    )
   );
 };
 
--- web/src/components/Layer.jsx
+++ web/src/components/Layer.jsx
@@ -2,6 +2,10 @@ const Layer = ({ zIndex }) => {
   return (
     <div className="layer-container" style={{ zIndex }}>
       <div className="layer" />
+      <div className="layer-text-container">
+        <div className="main">Bioassay Explained</div>
+        <div className="subtext">5th grade level, labs explanation</div>
+      </div>
     </div>
   );
 };
